2d14ff530478c16fab9c62ae7ac68feddab23c55
lexxmark <lexxmark@da3578c4-ce5d-4d2c-aa0c-bbd0a991affc>
2011-11-24 22:32:35 +0000

    move common code to common_lib project
    git-svn-id: svn://svn.code.sf.net/p/winflexbison/code/trunk@17 da3578c4-ce5d-4d2c-aa0c-bbd0a991affc

---
+++ common/misc/system.h
@@ -0,0 +1,245 @@
+/* System-dependent definitions.
+
+   Copyright (C) 2000-2007, 2009-2011 Free Software Foundation, Inc.
+
+   This program is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program.  If not, see <http://www.gnu.org/licenses/>.  */
+
+#ifndef COMMON_SYSTEM_H
+#define COMMON_SYSTEM_H
+
+#include <xalloc.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+/*------.
+| NLS.  |
+`------*/
+
+#include <locale.h>
+
+#include <gettext.h>
+#define _(Msgid)  gettext (Msgid)
+#define N_(Msgid) (Msgid)
+
+
+#ifndef __attribute__
+/* This feature is available in gcc versions 2.5 and later.  */
+# if (! defined __GNUC__ || __GNUC__ < 2 \
+      || (__GNUC__ == 2 && __GNUC_MINOR__ < 5) || __STRICT_ANSI__)
+#  define __attribute__(Spec) /* empty */
+# endif
+#endif
+
+/* The __-protected variants of `format' and `printf' attributes
+   are accepted by gcc versions 2.6.4 (effectively 2.7) and later.  */
+#if __GNUC__ < 2 || (__GNUC__ == 2 && __GNUC_MINOR__ < 7)
+# define __format__ format
+# define __printf__ printf
+#endif
+
+#ifndef ATTRIBUTE_NORETURN
+# define ATTRIBUTE_NORETURN __attribute__ ((__noreturn__))
+#endif
+
+#ifndef ATTRIBUTE_UNUSED
+# define ATTRIBUTE_UNUSED __attribute__ ((__unused__))
+#endif
+
+#if 0
+/* flex 2.5.31 gratutiously defines macros like INT8_MIN.  But this
+   runs afoul of pre-C99 compilers that have <inttypes.h> or
+   <stdint.h>, which are included below if available.  It also runs
+   afoul of pre-C99 compilers that define these macros in <limits.h>.  */
+#if ! defined __STDC_VERSION__ || __STDC_VERSION__ < 199901
+# undef INT8_MIN
+# undef INT16_MIN
+# undef INT32_MIN
+# undef INT8_MAX
+# undef INT16_MAX
+# undef UINT8_MAX
+# undef INT32_MAX
+# undef UINT16_MAX
+# undef UINT32_MAX
+#endif
+
+#include <limits.h>
+#include <stddef.h>
+#include <string.h>
+
+#if HAVE_SYS_TYPES_H
+# include <sys/types.h>
+#endif
+
+//#include <unistd.h>
+//#include <inttypes.h>
+
+#ifndef UINTPTR_MAX
+/* This isn't perfect, but it's good enough for Bison, which needs
+   only to hash pointers.  */
+typedef size_t uintptr_t;
+#endif
+
+
+/*---------.
+| Gnulib.  |
+`---------*/
+
+//#include <unlocked-io.h>
+#include <verify.h>
+#include <stdio.h>
+
+
+/*-----------------.
+| GCC extensions.  |
+`-----------------*/
+
+/* Use PACIFY_CC to indicate that Code is unimportant to the logic of Bison
+   but that it is necessary for suppressing compiler warnings.  For example,
+   Code might be a variable initializer that's always overwritten before the
+   variable is used.
+
+   PACIFY_CC is intended to be useful only as a comment as it does not alter
+   Code.  It is tempting to redefine PACIFY_CC so that it will suppress Code
+   when configuring without --enable-gcc-warnings.  However, that would mean
+   that, for maintainers, Bison would compile with potentially less warnings
+   and safer logic than it would for users.  Due to the overhead of M4,
+   suppressing Code is unlikely to offer any significant improvement in
+   Bison's performance anyway.  */
+#define PACIFY_CC(Code) Code
+
+#define FUNCTION_PRINT() fprintf (stderr, "%s: ", __func__)
+
+
+
+/*-----------.
+| Booleans.  |
+`-----------*/
+
+#include <stdbool.h>
+
+
+
+/*-------------.
+| Assertions.  |
+`-------------*/
+
+/* In the past, Bison defined aver to simply invoke abort in the case of
+   a failed assertion.  The rationale was that <assert.h>'s assertions
+   were too heavyweight and could be disabled too easily.  See
+   discussions at
+   <http://lists.gnu.org/archive/html/bison-patches/2006-01/msg00080.html>
+   <http://lists.gnu.org/archive/html/bison-patches/2006-09/msg00111.html>.
+
+   However, normal assert output can be helpful during development and
+   in bug reports from users.  Moreover, it's not clear now that
+   <assert.h>'s assertions are significantly heavyweight.  Finally, if
+   users want to experiment with disabling assertions, it's debatable
+   whether it's our responsibility to stop them.  See discussion
+   starting at
+   <http://lists.gnu.org/archive/html/bison-patches/2009-09/msg00013.html>.
+
+   For now, we use assert but we call it aver throughout Bison in case
+   we later wish to try another scheme.
+*/
+#include <assert.h>
+#define aver assert
+
+
+/*-----------.
+| Obstacks.  |
+`-----------*/
+
+#define obstack_chunk_alloc xmalloc
+#define obstack_chunk_free  free
+#include <obstack.h>
+
+#define obstack_sgrow(Obs, Str) \
+  obstack_grow (Obs, Str, strlen (Str))
+
+#define obstack_fgrow1(Obs, Format, Arg1)	\
+do {						\
+  char buf[4096];				\
+  sprintf (buf, Format, Arg1);			\
+  obstack_grow (Obs, buf, strlen (buf));	\
+} while (0)
+
+#define obstack_fgrow2(Obs, Format, Arg1, Arg2)	\
+do {						\
+  char buf[4096];				\
+  sprintf (buf, Format, Arg1, Arg2);		\
+  obstack_grow (Obs, buf, strlen (buf));	\
+} while (0)
+
+#define obstack_fgrow3(Obs, Format, Arg1, Arg2, Arg3)	\
+do {							\
+  char buf[4096];					\
+  sprintf (buf, Format, Arg1, Arg2, Arg3);		\
+  obstack_grow (Obs, buf, strlen (buf));		\
+} while (0)
+
+#define obstack_fgrow4(Obs, Format, Arg1, Arg2, Arg3, Arg4)	\
+do {								\
+  char buf[4096];						\
+  sprintf (buf, Format, Arg1, Arg2, Arg3, Arg4);		\
+  obstack_grow (Obs, buf, strlen (buf));			\
+} while (0)
+
+
+#include <mbstring.h>
+
+/*-----------------------------------------.
+| Extensions to use for the output files.  |
+`-----------------------------------------*/
+
+#ifndef OUTPUT_EXT
+# define OUTPUT_EXT ".output"
+#endif
+
+#ifndef TAB_EXT
+# define TAB_EXT ".tab"
+#endif
+
+#ifndef DEFAULT_TMPDIR
+# define DEFAULT_TMPDIR "/tmp"
+#endif
+
+
+
+/*---------------------.
+| Free a linked list.  |
+`---------------------*/
+
+#define LIST_FREE(Type, List)			\
+do {						\
+  Type *_node, *_next;				\
+  for (_node = List; _node; _node = _next)	\
+    {						\
+      _next = _node->next;			\
+      free (_node);				\
+    }						\
+} while (0)
+
+
+/*---------------------------------------------.
+| Debugging memory allocation (must be last).  |
+`---------------------------------------------*/
+
+# if WITH_DMALLOC
+#  define DMALLOC_FUNC_CHECK
+#  include <dmalloc.h>
+# endif /* WITH_DMALLOC */
+
+#endif
+
+#endif  /* ! COMMON_SYSTEM_H */
