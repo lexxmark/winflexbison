5dcd330d4fe73c32a3a6253e538d721a756669af
jlehtniemi-broadsoft <jlehtniemi@broadsoft.com>
2016-08-16 10:25:52 +0300

    Add checksum for Chocolatey download
    After recent policy change checksum is required for all Chocolatey packages.

--- chocolatey/tools/chocolateyInstall.ps1
+++ chocolatey/tools/chocolateyInstall.ps1
@@ -1,2 +1,6 @@
 # download and unpack a zip file
-Install-ChocolateyZipPackage "winflexbison" "http://downloads.sourceforge.net/project/winflexbison/win_flex_bison-2.4.1.zip" "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+Install-ChocolateyZipPackage `
+  -PackageName "winflexbison" `
+  -Url "http://downloads.sourceforge.net/project/winflexbison/win_flex_bison-2.4.1.zip" `
+  -Checksum "1fd2690fba8b1f342cc500c7a5fd67fe" `
+  -UnzipLocation "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
