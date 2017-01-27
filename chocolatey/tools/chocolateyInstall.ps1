d9ea860616bfe4375d94a03b948111ec59b7dd34
Alex Zhondin <lexxmark.dev@gmail.com>
2017-01-27 01:07:49 -0700

    update chocolatey

--- chocolatey/tools/chocolateyInstall.ps1
+++ chocolatey/tools/chocolateyInstall.ps1
@@ -1,7 +1,7 @@
 # download and unpack a zip file
 Install-ChocolateyZipPackage `
   -PackageName "winflexbison3" `
-  -Url "http://sourceforge.net/projects/winflexbison/files/old_versions/win_flex_bison-2.5.6.zip" `
-  -Checksum "6083985039A45D450946802D032DBCA739A4BC2D26F2F072B61D53CA48110981" `
+  -Url "http://sourceforge.net/projects/winflexbison/files/old_versions/win_flex_bison-2.5.7.zip" `
+  -Checksum "79992E198547DAF71DEC9B4AC80EDC28F5AA9C23DD74246F97C9BDF46190CA33" `
   -ChecksumType "sha256" `
   -UnzipLocation "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
