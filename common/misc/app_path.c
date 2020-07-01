c48cbf8494b790b950f79dd6671c47e16e5aa653
Christoph Cullmann <cullmann@kde.org>
2020-07-01 16:42:54 +0200

    use explicitly the ANSI variant of GetModuleFileName
    without this, the tool will miscompile with -DUNICODE set

--- common/misc/app_path.c
+++ common/misc/app_path.c
@@ -5,7 +5,7 @@ static char app_path[APP_PATH_LEN+1];
 
 const char* get_app_path()
 {
-	DWORD res = GetModuleFileName(NULL, app_path, APP_PATH_LEN);
+	DWORD res = GetModuleFileNameA(NULL, app_path, APP_PATH_LEN);
 
 	if (res == 0 || res == APP_PATH_LEN)
 		return 0;
