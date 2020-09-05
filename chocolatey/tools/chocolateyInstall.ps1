98a7c9ed44e2e60c727a921940b559c4ffb35719
Alex Zhondin <lexxmark.dev@gmail.com>
2020-09-04 20:56:27 -0700

    fixed choco package

--- chocolatey/tools/chocolateyInstall.ps1
+++ chocolatey/tools/chocolateyInstall.ps1
@@ -1,7 +1,7 @@
 # download and unpack a zip file
 Install-ChocolateyZipPackage `
   -PackageName "winflexbison3" `
-  -Url "https://github.com/lexxmark/winflexbison/releases/download/v2.5.18/win_flex_bison-2.5.18.zip" `
-  -Checksum "095CF65CB3F12EE5888022F93109ACBE6264E5F18F6FFCE0BDA77FEB31B65BD8" `
+  -Url "https://github.com/lexxmark/winflexbison/releases/download/v2.5.23/win_flex_bison-2.5.23.zip" `
+  -Checksum "6AA5C8EA662DA1550020A5804C28BE63FFAA53486DA9F6842E24C379EC422DFC" `
   -ChecksumType "sha256" `
   -UnzipLocation "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
--- chocolatey/tools/chocolateyInstallWinFlexBison2.ps1
+++
@@ -1,7 +0,0 @@
-# download and unpack a zip file
-Install-ChocolateyZipPackage `
-  -PackageName "winflexbison" `
-  -Url "https://github.com/lexxmark/winflexbison/releases/download/v2.4.12/win_flex_bison-2.4.12.zip" `
-  -Checksum "5D93FF7A69379FCF521601B2163FCD35D54E018CE42671C718B5CD9B134626FF" `
-  -ChecksumType "sha256" `
-  -UnzipLocation "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
