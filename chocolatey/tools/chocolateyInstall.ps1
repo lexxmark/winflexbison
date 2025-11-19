2040672b690523e6a5b2e51480d90132a39d4c76
Michael Keck <git@cr0ydon.com>
2025-11-19 22:02:14 +0100

    Fix CI, include all licenses, simplify CMake (#102)
    * AppVeyor: Rewrite to only use CMake commands
    
    * Chocolatey: Update version to current release
    
    * Readmes: Point to GitHub instead of Sourceforge
    
    * Readme: CMake is now mandatory
    
    Since 1176da2adaf972b2216b1ac8dd611e128fe80bc5
    
    * Expand .gitignore
    
    * nit: line-endings
    
    This commit is basically just a git rebase leftover. Originally updated CMake versions in the sub-projects which is already merged now.
    
    * Readmes: VS2017 is the minimum supported version
    
    * CPack: Actually install all license files
    
    This change, plus the changes applied in #84 fixes #94
    
    * CMake: $PROJECT_NAME is built-in
    
    No need to define this manually
    
    https://cmake.org/cmake/help/v3.10/variable/PROJECT_NAME.html
    
    * CMake: The entire project is C only
    
    This is an implicit performance optimization as CMake does not have to check for C++ capabilities in the setup
    
    * CPack: Rename default dev package from master to dev-PROJECT_VERSION
    
    * CMake: Simplify setting default CMAKE_BUILD_TYPE
    
    * CMake: More explicit syntax to specific project language
    
    * Readme: Simplify
    
    * AppVeyor: Include "vs" in artifact names

--- chocolatey/tools/chocolateyInstall.ps1
+++ chocolatey/tools/chocolateyInstall.ps1
@@ -1,7 +1,7 @@
 # download and unpack a zip file
 Install-ChocolateyZipPackage `
   -PackageName "winflexbison3" `
-  -Url "https://github.com/lexxmark/winflexbison/releases/download/v2.5.24/win_flex_bison-2.5.24.zip" `
-  -Checksum "39C6086CE211D5415500ACC5ED2D8939861CA1696AEE48909C7F6DAF5122B505" `
+  -Url "https://github.com/lexxmark/winflexbison/releases/download/v2.5.25/win_flex_bison-2.5.25.zip" `
+  -Checksum "8D324B62BE33604B2C45AD1DD34AB93D722534448F55A16CA7292DE32B6AC135" `
   -ChecksumType "sha256" `
   -UnzipLocation "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
